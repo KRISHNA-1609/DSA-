69d2c999555f3cdb685e078fd5f57bb513ce415e
Krishna Shonka <24shonkak@rbunagpur.in>
2026-09-04 18:16:22 +0000

    Fix Java programs and run verification

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class BinarySearch {
     public static int BinarySearch(int numbers[],int key){
         int start = 0, end = numbers.length-1;
@@ -19,7 +20,7 @@ public class BinarySearch {
         }
         return -1;
     }
-    public static void main(Str[] args) {
+    public static void main(String[] args) {
         int numbers[] = {2,4,6,8,10,12,14};
         int key =12;
 
---
+++ BuyAndSellStocks.java
@@ -0,0 +1,21 @@
+public class BuyAndSellStocks {
+    public static int BuyAndSellStocks(int prices[]){
+        int buyPrice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+
+        for(int i=0;i<prices.length;i++){
+            if(buyPrice<prices[i]){
+                int profit = prices[i] - buyPrice;
+                maxprofit = Math.max(maxprofit,profit);
+            }
+            else{
+                buyPrice = prices[i];
+            }
+        }
+        return maxprofit;
+    }
+    public static void main(String[] args) {
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println(BuyAndSellStocks(prices));
+    }
+}
