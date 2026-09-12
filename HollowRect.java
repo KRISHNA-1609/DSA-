93fd82be85387083b5a15a3f9ab8af53e9d591b4
Krishna Shonka <24shonkak@rbunagpur.in>
2026-09-12 17:12:38 +0000

    Hollow Rectangle

---
+++ HollowRect.java
@@ -0,0 +1,24 @@
+public class HollowRect {
+    public static void HollowRect(int totRows, int totCols){
+        for(int i=1;i<=totRows;i++){
+            //inner loop->columns
+            for(int j=1;j<=totCols;j++){
+                //cell-(i,j)
+                if(i==1 || i==totRows || j==1 || j==totCols){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String [] args){
+        
+      HollowRect(4,5);
+
+        
+    }
+}
